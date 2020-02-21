a38a21fa26c0b6da3bee5fe1ba143ce722464858
Ivan Lee <ivnyle@gmail.com>
2020-02-21 10:13:43 -0800

    optimized NumberToPattern

--- week-1/NumberToPattern.py
+++ week-1/NumberToPattern.py
@@ -1,22 +1,22 @@
 def NumberToPattern(index, k):
-    output_string = ''
-    b = 4**(k-1)
-    while index > 0:
-        output_string += str(index // b)
-        index %= b
-        b //= 4
-    
     map = {'0': 'A', 
            '1': 'C', 
            '2': 'G', 
            '3': 'T'}
-    nstring = ''
-    for x in output_string:
-        nstring += map[x]
-    return nstring
+    output_string = ''
+    b = 4**(k-1)
+    while index > 0:
+        output_string += map[str(index // b)]
+        index %= b
+        b //= 4
+    return output_string
 
 index = 5437
 k = 7
+print(NumberToPattern(index, k))
+
+index = 5437
+k = 8
 print(NumberToPattern(index, k))
         
 
