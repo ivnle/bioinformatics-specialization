57af160bdeca05797c38aad88221c0031a26c6c6
Ivan Lee <ivnyle@gmail.com>
2020-02-23 17:11:29 -0800

    Implemented faster clumpfinding

--- week-1/ClumpFinding.py
+++ week-1/ClumpFinding.py
@@ -1,18 +1,30 @@
 from ComputingFrequencies import ComputingFrequencies
 from NumberToPattern import NumberToPattern
+from PatternToNumber import PatternToNumber
 
 def ClumpFinding(Genome: str, k: int, L: int, t: int) -> list:
     FrequentPatterns: list = []
     Clump: list = [0] * (4**k)
     n: int = len(Genome)
+    FrequencyArray: list = ComputingFrequencies(Genome[:L], k)
+    i: int; freq: int
+    for i, freq in enumerate(FrequencyArray):
+        if freq >= t:
+            Clump[i] = 1
     
-    i: int
-    for i in range(n - L + 1):
-        FrequencyArray: list = ComputingFrequencies(Genome[i:i+L], k)
-        j: int; freq: int
-        for j, freq in enumerate(FrequencyArray):
-            if freq >= t:
-                Clump[j] = 1
+    j: int
+    for j in range(1, n - L + 1):
+        
+        FirstPattern = Genome[j-1:j-1+k]
+        index = PatternToNumber(FirstPattern)
+        if FrequencyArray[index] > 0:
+            FrequencyArray[index] -= 1
+        
+        LastPattern = Genome[j+L-k:j+L]
+        index = PatternToNumber(LastPattern)
+        FrequencyArray[index] += 1        
+        if FrequencyArray[index] >= t:
+            Clump[index] = 1
     
     z: int; is_clump: int
     for z, is_clump in enumerate(Clump):
